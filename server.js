df9281cffba1224f2584d7a782027356455f6f9e
la-ki <layarkostic94@gmail.com>
2020-05-16 14:50:25 +0200

    Server Configuration

--- server.js
+++ server.js
@@ -1,5 +1,30 @@
 const express = require('express');
+const path = require('path');
+const dotenv = require('dotenv');
+const color = require('colors');
+const morgan = require('morgan');
+
+dotenv.config({path: './config/config.env'});
 
 const app = express();
 
-app.listen();
+app.use(express.json());
+
+if(process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
+
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
+const PORT = process.env.PORT || 5001;
+
+
+app.listen(PORT, console.log(`
+    Server running in ${process.env.NODE_ENV} mode on port ${PORT}
+`.yellow.bold));
