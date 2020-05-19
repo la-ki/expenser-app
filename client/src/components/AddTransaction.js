ed605b0c1cf19bc0283177d45af764b2daa610ee
la-ki <layarkostic94@gmail.com>
2020-05-19 14:48:30 +0200

    Added button for adding transactions

--- client/src/components/AddTransaction.js
+++ client/src/components/AddTransaction.js
@@ -1,44 +1,39 @@
-import React, {useState, useContext} from 'react';
+import React, {useState, useContext} from 'react'
 import { GlobalContext } from '../context/GlobalState';
 
 export const AddTransaction = () => {
+  const [text, setText] = useState('');
+  const [amount, setAmount] = useState(0);
 
+  const { addTransaction } = useContext(GlobalContext);
 
-    const [text, setText] = useState('');
-    const [amount, setAmount] = useState(0);
+  const onSubmit = e => {
+    e.preventDefault();
 
-    const {AddTransaction} = useContext(GlobalContext);
-
-    const onSubmit = e => {
-
-        e.preventDefault();
-
-        const newTransaction = {
-
-            text,
-            amount: +amount
-
-        }
-
-        AddTransaction(newTransaction);
+    const newTransaction = {
+      text,
+      amount: +amount
     }
 
-    return (
-        <>
-            <h3>Dodaj Novu Transakciju</h3>
-            <form onSubmit={onSubmit}>
+    addTransaction(newTransaction);
+  }
 
-                <div className="form-control">
-                    <label htmlFor="text">Oznaka plaćanja</label>
-                    <input type="text" value={text} onChange={(e) => setText(e.target.value)} placeholder="Naziv transakcije..." />
-                </div>
-
-                <div className="form-control">
-                    <label htmlFor="amount">Iznos plaćanja (+ za potražnju, - za zaduženje)</label>
-                    <input type="number" value={amount} onChange={(e) => setAmount(e.target.value)} placeholder="Unesite iznos..." />
-                </div>
-
-            </form>
-        </>
-    )
+  return (
+    <>
+      <h3>Dodaj novu transakciju</h3>
+      <form onSubmit={onSubmit}>
+        <div className="form-control">
+          <label htmlFor="text">Oznaka plaćanja</label>
+          <input type="text" value={text} onChange={(e) => setText(e.target.value)} placeholder="Naziv transakcije..." />
+        </div>
+        <div className="form-control">
+          <label htmlFor="amount"
+            >Vrednost <br />
+            (negativno - zaduženje, pozitivno - potražnja)</label>
+          <input type="number" value={amount} onChange={(e) => setAmount(e.target.value)} placeholder="Unesite iznos..." />
+        </div>
+        <button className="btn">Dodaj Transakciju</button>
+      </form>
+    </>
+  )
 }
