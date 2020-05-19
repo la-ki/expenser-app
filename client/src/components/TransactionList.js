07925750c4cbc7a676098c9dc1a8a2a33c5a613c
la-ki <layarkostic94@gmail.com>
2020-05-19 14:03:40 +0200

    Added Global State and App Reducer

---
+++ client/src/components/TransactionList.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {Transaction} from './Transaction';
+
+export const TransactionList = () => {
+    return (
+        <>
+            <h3>Istorija Transakcija</h3>
+            <ul className="list">
+                
+            </ul>
+        </>
+    )
+}
