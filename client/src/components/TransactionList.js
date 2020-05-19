49c20e7c7919679d1e5d08ee6c2da26b9d5c7aa6
la-ki <layarkostic94@gmail.com>
2020-05-19 14:39:23 +0200

    Added TransactionList component

--- client/src/components/TransactionList.js
+++ client/src/components/TransactionList.js
@@ -1,12 +1,21 @@
-import React from 'react'
-import {Transaction} from './Transaction';
+import React, { useContext, useEffect } from 'react';
+import { Transaction } from './Transaction';
+import { GlobalContext } from '../context/GlobalState';
 
 export const TransactionList = () => {
+
+    const { transactions, getTransactions } = useContext(GlobalContext);
+
+    useEffect(() => {
+        getTransactions();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
     return (
         <>
             <h3>Istorija Transakcija</h3>
             <ul className="list">
-                
+                {transactions.map(transaction => (<Transaction key={transaction._id} transaction={transaction} />))}
             </ul>
         </>
     )
